799fc192b5a1f538e6157bb75d04bcc9c7f2eb0e
Douglas Giovanella <douglas.giovanella@asaas.com.br>
2023-10-21 14:34:28 -0300

    initial commit

---
+++ FlyoutTest/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace FlyoutTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		MainPage = new NavigationPage(new MainPage());
+	}
+}
+
---
+++ FlyoutTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace FlyoutTest;
+
+public partial class MainPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		flyoutPage.collectionView.SelectionChanged += OnSelectionChanged;
+	}
+
+	void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+		if (e.CurrentSelection.FirstOrDefault() is FlyoutPageItem item)
+		{
+				IsPresented = false;
+		}
+	}
+}
+
+
